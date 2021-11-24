4b9b3175e7ef2364d6ccfeae6b887f4b9e7427f5
AAAR-Salmon <65284522+AAAR-Salmon@users.noreply.github.com>
2021-11-24 20:40:11 +0900

    Add: slide-page要素にfirst属性を追加
    最初のページでは改ページをかけないように

--- elements/slide/page.js
+++ elements/slide/page.js
@@ -5,6 +5,9 @@ class SlidePage extends HTMLElement {
     const shadow = this.attachShadow({ mode: 'closed' });
     const wrapper = document.createElement('div');
     wrapper.classList.add('page-wrapper');
+    if (!this.getAttribute('first')) {
+      wrapper.classList.add('break-before');
+    }
     const link = document.createElement('link');
     link.setAttribute('rel', 'stylesheet');
     link.setAttribute('href', '/elements/slide/page.css');
