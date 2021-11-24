f18b9d8404043f40f03cde44987a4ed8df8be9ef
AAAR-Salmon <65284522+AAAR-Salmon@users.noreply.github.com>
2021-11-24 20:09:12 +0900

    Add: slide-page要素を実装

---
+++ elements/slide/page.js
@@ -0,0 +1,17 @@
+class SlidePage extends HTMLElement {
+  constructor() {
+    super();
+
+    const shadow = this.attachShadow({ mode: 'closed' });
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('page-wrapper');
+    const link = document.createElement('link');
+    link.setAttribute('rel', 'stylesheet');
+    link.setAttribute('href', '/elements/slide/page.css');
+    shadow.appendChild(link);
+    wrapper.append(...this.childNodes);
+    shadow.appendChild(wrapper);
+  }
+}
+
+customElements.define('slide-page', SlidePage);
