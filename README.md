e08350954f45116d38a9657d2d8defc050ef2297
AAAR-Salmon <65284522+AAAR-Salmon@users.noreply.github.com>
2021-11-25 20:09:03 +0900

    Add: PDFの生成を実装

--- README.md
+++ README.md
@@ -11,19 +11,24 @@
 - バックエンド
   - Ubuntu 20.04.3 LTS (WSL2 Linux 5.4.72)
   - Go 1.17.3
-  - Sass 1.43.5
 - フロントエンド
   - Windows10 20H2
   - Chrome 96.0.4664.45
+- ユーティリティ
+  - Node.js v16.13.0
+    - 使用モジュールはpackage.jsonを見よ
 
 ### コマンド
 
 ```bash
 # Compile sass
-npx sass assets/
+npm run sass
 
 # Start server
-go run main.go
+go run main.go &
+
+# Print to PDF
+npm run print-pdf -- slides/what-is-html/ -o what-is-html.pdf
 ```
 
 ## LICENSE
--- print-pdf.js
+++ print-pdf.js
@@ -1,13 +1,24 @@
-const Puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer');
+const commandLineArgs = require('command-line-args');
+
+const optionDefinitions = [
+  { name: 'path', defaultOption: true },
+  { name: 'baseUrl', alias: 'u', defaultValue: 'http://localhost:8888/' },
+  { name: 'output', alias: 'o' }
+];
+
+const options = commandLineArgs(optionDefinitions);
 
 (async () => {
-  const browser = await Puppeteer.launch();
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  await page.goto('http://localhost:8888/slides/what-is-html/');
+  const targetUrl = new URL(options.path, options.baseUrl);
+
+  await page.goto(targetUrl.toString());
 
   await page.pdf({
-    path: 'what-is-html.pdf',
+    path: options.output ?? 'page.pdf',
     width: '338.7mm',
     height: '190.5mm'
   });
